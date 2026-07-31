6e9a571309077985ca934f696f32255d12b168c5
Jo Ee <joeewong1276@gmail.com>
2026-07-31 17:50:39 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 import 'firebase_options.dart';
